c3cb6435a5b8a154dcff092faf5b4c3bfb8cf060
sgrieble <sophie.grieble@gmail.com>
2024-07-13 11:56:01 +0200

    websearch onclick of subcategories

--- app/main.py
+++ app/main.py
@@ -10,7 +10,10 @@ def search_engine_user_interface():
 
 @app.route('/serp', methods=['POST'])
 def get_result_list():#
-    query = request.form.get('query')
+    try:
+        query = request.form.get('query')
+    except:
+        query = request.get_json().query
     print(query)
     result_list = [{"link": "https://d3rlpy.readthedocs.io/en/v2.5.0/", "title": "d3rlpy - An offline deep reinforcement learning library", "content": "d3rlpy provides state-of-the-art offline deep reinforcement learning algorithms through out-of-the-box scikit-learn-style APIs. Unlike other RL libraries, the provided algorithms can achieve extremely powerful performance beyond their papers via several tweaks."},
                    {"link": "https://ovidius.uni-tuebingen.de/ilias3/login.php?target=&client_id=pr02&auth_stat=", "title": "ILIAS Uni Tübingen", "content": "Login mit zentraler Universitäts-Kennung Sie betreten die ILIAS Plattform für Mitglieder der Universität Tübingen. Studierende finden hier Materialien für ihre Veranstaltungen. Lehrenden bietet ILIAS effektive Verwaltungs-, Kommunikations- und Kooperationswerkzeuge. Kontakt: esc@ub.uni-tuebingen.de. Login mit zentraler Universitäts-Kennung Sie betreten die ILIAS Plattform für Mitglieder der Universität Tübingen. Studierende finden hier Materialien für ihre Veranstaltungen. Lehrenden bietet ILIAS effektive Verwaltungs-, Kommunikations- und Kooperationswerkzeuge. Kontakt: esc@ub.uni-tuebingen.de. Login mit zentraler Universitäts-Kennung Sie betreten die ILIAS Plattform für Mitglieder der Universität Tübingen. Studierende finden hier Materialien für ihre Veranstaltungen. Lehrenden bietet ILIAS effektive Verwaltungs-, Kommunikations- und Kooperationswerkzeuge. Kontakt: esc@ub.uni-tuebingen.de"},
